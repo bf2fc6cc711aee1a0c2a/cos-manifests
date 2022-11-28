ce9619b4e4497cd2b2c179c424e79412ee2cb492
Rinaldo Pitzer Júnior <16694899+rinaldodev@users.noreply.github.com>
2022-11-28 09:57:58 -0300

    fix: adapt fetching previous tag to new pipeline

--- hack/fetch-previous-tag.sh
+++ hack/fetch-previous-tag.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-git fetch --tags
-git tag | tr - \~ | sort -V | tr \~ - | tail -2 | head -1 2>/dev/null
+function fetchTag()
+{
+    git clone -n https://github.com/bf2fc6cc711aee1a0c2a/cos-fleetshard.git ./fleetshard-for-tags
+    cd ./fleetshard-for-tags/ || exit
+    git fetch --tags
+    result=$(git tag | tr - \~ | sort -V | tr \~ - | tail -2 | head -1 2>/dev/null)
+    cd ..
+    rm -rf ./fleetshard-for-tags
+    echo "$result"
+}
+
+result=$(fetchTag)
+echo $result
